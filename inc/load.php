eea5aa530ca13e9c0296c55b2607475753441286
bordoni <bordoni@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-07 15:30:36 +0000

    Update to commit 2b8dc72 from git@github.com:bordoni/fakerpress.git
    Includes the following commit(s):
    
    commit 2b8dc72078ecd2de6f18d922b8c82c67af4c6933
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Readme Changes for 0.1.6
    
    M	readme.md
    M	readme.txt
    
    commit 287cab293e313b439449622b774809ef4ef0bb69
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Finishing touches to the 0.1.6 version resolves #50
    
    M	inc/init/settings.php
    M	inc/load.php
    M	ui/js/fields.js
    
    commit d032c264b16f17fb607f34dfd7d47998a0c38a52
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Version 0.1.6 and Fixing bugs for the erase method
    
    M	bin/phpcs.ruleset.xml
    D	classes/class-fp-dates.php
    A	classes/dates.php
    M	classes/plugin.php
    M	fakerpress.php
    M	inc/init/settings.php
    M	inc/load.php
    M	modules/term.php
    M	readme.md
    M	readme.txt
    
    commit 772497d83c8d4ae464c200ca02d2d06a3c874a66
    Merge: cc86763 8afd048
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Merge branch 'master' of github.com:iryz/fakerpress
    
    commit cc86763430f0b8deac85784cd6557f1a19b2baf0
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Update the text about ranges, very good input from a user: https://wordpress.org/support/topic/simplify-or-explain-the-multiple-number-inputs-for-quantity
    
    M	view/comments.php
    M	view/posts.php
    M	view/terms.php
    M	view/users.php
    
    commit 8afd048c3b9d688052d36ea6a349d8570709dbcf
    Merge: 82969f0 e7d1233
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Merge pull request #48 from iryz/issue/45
    
    commit 82969f0304a35efe0412974963dce56c46b77409
    Merge: 74e2e55 a473683
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Merge branch 'master' of github.com:iryz/fakerpress
    
    commit 74e2e55cc60cba2c9cb9d0e2fbed0d08cbdcebe3
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Transifex only uses .po files instead of .pot
    
    A	l10n/fakerpress.po
    D	l10n/fakerpress.pot
    
    commit a4736830f040f3798d8d40e90df8140e74b4e29a
    Merge: 3478e7a 957fcce
    Author: Luan Cuba <luancuba9@gmail.com>
    
        Merge pull request #49 from iryz/hotfix/delete_generated_data
    
    commit 957fcceb7aec038d4b29aacbb4da46644c451755
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Fix a bug where just a few posts are been deleted at once, delete everything as expected
    
    M	inc/init/settings.php
    M	view/posts.php
    
    commit e7d12337ea655aeb62266421c7290e2b0f57826c
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Improve default elements on the HTML fields
    
    M	view/comments.php
    M	view/users.php
    
    commit 0e235093554b81f0483576c1f7e143dfac22ab6a
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Allowing WordPress more in a randomic way
    
    M	providers/html.php
    
    commit de806a8d4437d5968b74aad9bb154421099700a4
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Improving the HTML provider
    
    M	providers/html.php
    
    commit 3478e7a1b9f8e986fd47a79e5a86ed96d38c7960
    Merge: b0d194e 32b5a5e
    Author: Gustavo Bordoni <bordoni.dev@gmail.com>
    
        Merge pull request #46 from iryz/issue/39
    
    commit 32b5a5eab438627b08dee8778a4138084dd7b51b
    Author: Luan Cuba <luancuba9@gmail.com>
    
        Added HTML Tags to User Description and Comment Content
    
    M	inc/init/comments.php
    M	inc/init/users.php
    M	modules/comment.php
    M	modules/user.php
    M	providers/html.php
    M	providers/wp-comment.php
    M	providers/wp-user.php
    M	view/comments.php
    M	view/posts.php
    M	view/terms.php
    M	view/users.php
    
    
    git-svn-id: https://plugins.svn.wordpress.org/fakerpress/trunk@1107670 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/load.php
+++ inc/load.php
@@ -11,10 +11,12 @@ $FakerPress = new Plugin;
 require_once Plugin::path( 'inc/vendor/Faker/src/autoload.php' );
 
 // Include the Carbon class to deal with dates
-require_once Plugin::path( 'inc/vendor/Carbon/Carbon.php' );
+if ( ! class_exists( '\Carbon' ) ){
+	require_once Plugin::path( 'inc/vendor/Carbon/Carbon.php' );
+}
 
 // Include the Carbon class to deal with dates
-require_once Plugin::path( 'classes/class-fp-dates.php' );
+require_once Plugin::path( 'classes/dates.php' );
 
 // Require our Administration Class
 require_once Plugin::path( 'classes/filter.php' );
--- view/comments.php
+++ view/comments.php
@@ -4,6 +4,8 @@ namespace FakerPress;
 // Mount the carbon values for dates
 $_json_date_selection_output = Dates::get_intervals();
 
+$_elements = array_merge( \Faker\Provider\HTML::$sets['header'], \Faker\Provider\HTML::$sets['list'], \Faker\Provider\HTML::$sets['block'] );
+
 ?>
 
 <div class='wrap'>
@@ -17,11 +19,27 @@ $_json_date_selection_output = Dates::get_intervals();
 					<th scope="row"><label for="fakerpress_qty"><?php _e( 'Quantity', 'fakerpress' ); ?></label></th>
 					<td>
 						<div id="fakerpress[qty]" class='fakerpress_qty_range'>
-							<input style='width: 90px;' class='qty-range-min' type='number' max='25' min='1' placeholder='<?php esc_attr_e( 'e.g.: 0', 'fakerpress' ); ?>' value='' name='fakerpress_qty_min' />
+							<input style='width: 90px;' class='qty-range-min' type='number' max='25' min='1' placeholder='<?php esc_attr_e( 'e.g.: 1', 'fakerpress' ); ?>' value='' name='fakerpress_qty_min' />
 							<div class="dashicons dashicons-arrow-right-alt2 dashicon-date" style="display: inline-block;"></div>
 							<input style='width: 90px;' class='qty-range-max' type='number' max='25' min='1' placeholder='<?php esc_attr_e( 'e.g.: 10', 'fakerpress' ); ?>' value='' name='fakerpress_qty_max' disabled/>
 						</div>
-						<p class="description"><?php _e( 'The range of Comments you want to generate on this request', 'fakerpress' ); ?></p>
+						<p class="description"><?php _e( 'How many comments should be generated, use both fields to get a randomized number of comments within the given range.', 'fakerpress' ); ?></p>
+					</td>
+				</tr>
+				<tr class='fk-field-container field-container-comment_content_use_html'>
+					<th scope="row"><label for="fakerpress_comment_content_use_html"><?php _e( 'Use HTML', 'fakerpress' ); ?></label></th>
+					<td>
+						<input type='checkbox' style="margin-top: -3px;" name='fakerpress_comment_content_use_html' checked />
+						<p style='display: inline-block;' class="description"><?php _e( 'Use HTML on your randomized comment content?', 'fakerpress' ); ?></p>
+					</td>
+				</tr>
+				<tr class='fk-field-container fk-field-dependent' data-fk-depends=".field-container-comment_content_use_html input" data-fk-condition='true'>
+					<th scope="row"><label for="fakerpress_comment_content_html_tags"><?php _e( 'HTML tags', 'fakerpress' ); ?></label></th>
+					<td>
+						<div id="fakerpress[comment_content_html_tags]">
+							<input type='hidden' class='field-select2-tags' name='fakerpress_comment_content_html_tags' value='<?php echo esc_attr( implode( ',', $_elements ) ); ?>' data-tags='<?php echo json_encode( $_elements ); ?>' />
+						</div>
+						<p class="description"><?php _e( 'Select the group of tags that can be selected to print on the Comment Content.', 'fakerpress' ); ?></p>
 					</td>
 				</tr>
 				<tr>
@@ -30,8 +48,8 @@ $_json_date_selection_output = Dates::get_intervals();
 						<div class='fakerpress-range-group'>
 							<select id="fakerpress_interval_date" class='field-date-selection' data-placeholder='<?php esc_attr_e( 'Select an Interval', 'fakerpress' ); ?>' style="margin-right: 5px; margin-top: -4px;">
 								<option></option>;
-								<?php foreach ($_json_date_selection_output as $option) { ?>
-								<option data-min="<?php echo date( 'm/d/Y', strtotime( $option['min'] ) ); ?>" data-max="<?php echo date( 'm/d/Y', strtotime( $option['max'] ) ); ?>" value="<?php echo $option['text']; ?>"><?php echo $option['text']; ?></option>
+								<?php foreach ( $_json_date_selection_output as $option ) { ?>
+								<option data-min="<?php echo esc_attr( date( 'm/d/Y', strtotime( $option['min'] ) ) ); ?>" data-max="<?php echo esc_attr( date( 'm/d/Y', strtotime( $option['max'] ) ) ); ?>" value="<?php echo esc_attr( $option['text'] ); ?>"><?php echo esc_attr( $option['text'] ); ?></option>
 								<?php } ?>
 							</select>
 
